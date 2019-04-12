eea06d96f8f48c5b02255ba09491b183236fa82b
Roshan Adhikari <nahsorad@gmail.com>
2019-04-12 17:59:58 +0545

    Fix error message

--- script/script.js
+++ script/script.js
@@ -4,61 +4,67 @@ function makeRequest(){
     let pkg = document.getElementById("pkg-name").value; 
     let statValue = document.querySelector('input[name="stat-value"]:checked').value;
     let statFor = document.querySelector('input[name="stat-for"]:checked').value;
-    if(statFor == "none"){
-        let fromDate = document.getElementById("from-date").value;
-        let toDate = document.getElementById("to-date").value;
-        url='https://api.npmjs.org/downloads/' + statValue + '/' + fromDate + ':' + toDate + '/' + pkg;
+    let fromDate = document.getElementById("from-date").value;
+    let toDate = document.getElementById("to-date").value;
+    let statsModal = document.getElementById("stats-modal");
+    let fetchData = document.getElementsByClassName("fetch-data")[0];
+    if(pkg.trim() == ""){
+        statsModal.style.display = "block";
+        fetchData.innerText = "You must enter a package name!";
     }
     else{
-        url='https://api.npmjs.org/downloads/' + statValue + '/' + statFor + '/' + pkg;
-    }
-
-    http.open("GET", url);
-    http.send();
-    http.onreadystatechange = (e) => {
-        let statsModal = document.getElementById("stats-modal");
-        let fetchData = document.getElementsByClassName("fetch-data")[0];
-        let statValue = document.querySelector('input[name="stat-value"]:checked').value;
-
-        statsModal.style.display = "block";
-        fetchData.innerText = "";
-
-        if(statValue == "point"){
-            var json = JSON.parse(http.responseText);
-            for(var key in json){
-                if(json.hasOwnProperty(key)){
-                    fetchData.innerHTML += key.bold() + ": " + json[key] + "<br>";
-                }
-            }
+        if(statFor == "none"){
+            url='https://api.npmjs.org/downloads/' + statValue + '/' + fromDate + ':' + toDate + '/' + pkg;
         }
-        else if(statValue == "range"){
-            var json = JSON.parse(http.responseText);
-            for(var key in json){
-                if(json.hasOwnProperty(key)){
-                    if(key != "downloads"){
+        else{
+            url='https://api.npmjs.org/downloads/' + statValue + '/' + statFor + '/' + pkg;
+        }
+    
+        http.open("GET", url);
+        http.send();
+        http.onreadystatechange = (e) => {
+            let statValue = document.querySelector('input[name="stat-value"]:checked').value;
+    
+            statsModal.style.display = "block";
+            fetchData.innerText = "";
+    
+            if(statValue == "point"){
+                var json = JSON.parse(http.responseText);
+                for(var key in json){
+                    if(json.hasOwnProperty(key)){
                         fetchData.innerHTML += key.bold() + ": " + json[key] + "<br>";
                     }
-                    else{
-                        var jsonArray = json.downloads;
-                        jsonArray.forEach((nestedJson) => {
-                            var needLineBreak = 0;
-                            for(var nestedKey in nestedJson){
-                                fetchData.innerHTML += nestedKey.bold() + ": " + nestedJson[nestedKey] + " ";
-                                
-                                // Check if line break should be inserted
-                                needLineBreak = (needLineBreak + 1) % 2;
-                                if(needLineBreak == 0)
-                                // Concatenate every two lines: for downloads: <num> and day:<date>
-                                // Then after concatenating the two lines, break a line - repeat
-                                    fetchData.innerHTML += "<br>";
-                            }
-                        });
-                    }
                 }
             }
-        }   
-        
-    }
+            else if(statValue == "range"){
+                var json = JSON.parse(http.responseText);
+                for(var key in json){
+                    if(json.hasOwnProperty(key)){
+                        if(key != "downloads"){
+                            fetchData.innerHTML += key.bold() + ": " + json[key] + "<br>";
+                        }
+                        else{
+                            var jsonArray = json.downloads;
+                            jsonArray.forEach((nestedJson) => {
+                                var needLineBreak = 0;
+                                for(var nestedKey in nestedJson){
+                                    fetchData.innerHTML += nestedKey.bold() + ": " + nestedJson[nestedKey] + " ";
+                                    
+                                    // Check if line break should be inserted
+                                    needLineBreak = (needLineBreak + 1) % 2;
+                                    if(needLineBreak == 0)
+                                    // Concatenate every two lines: for downloads: <num> and day:<date>
+                                    // Then after concatenating the two lines, break a line - repeat
+                                        fetchData.innerHTML += "<br>";
+                                }
+                            });
+                        }
+                    }
+                }
+            }   
+            
+        }
+    } 
 }
 
 function toggleFromToDate(element){
